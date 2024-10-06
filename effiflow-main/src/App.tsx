d3e83f212c7eeac113f5ba2526b009146087edcb
ProductPadi <productpadi50@gmail.com>
2024-10-06 13:11:24 +0100

    Delete effiflow-main directory

--- effiflow-main/src/App.tsx
+++
@@ -1,9 +0,0 @@
-import { RouterProvider } from 'react-router-dom'
-import './App.css'
-import routes from './router'
-
-function App() {
-    return <RouterProvider router={routes} />
-}
-
-export default App
